5edc1df1502fbc4601d775034f2898939a490544
Stephen L <logunov.stepan2001@gmail.com>
2023-03-13 19:26:20 +0700

    Added status code converter

---
+++ IntegorSharedErrorHandlers/Converters/StatusCodeErrorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.WebUtilities;
+
+using IntegorErrorsHandling;
+using IntegorErrorsHandling.Converters;
+
+namespace IntegorSharedErrorHandlers.Converters
+{
+	public class StatusCodeErrorConverter : ConverterBase, IErrorConverter<int>
+	{
+		public IErrorConvertationResult? Convert(int statusCode)
+		{
+			string message = ReasonPhrases.GetReasonPhrase(statusCode);
+
+			if (string.IsNullOrEmpty(message))
+				return null;
+
+			return Single(message);
+		}
+	}
+}
