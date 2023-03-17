f33ed5f79bc309e6e37538d06b60c9e6e7f46c82
Stephen L <logunov.stepan2001@gmail.com>
2023-03-17 21:38:34 +0700

    Set correct versioning

--- IntegorSharedErrorHandlers/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompanyAttribute("IntegorSharedErrorHandlers")]
-[assembly: AssemblyConfigurationAttribute("Release")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: AssemblyProductAttribute("IntegorSharedErrorHandlers")]
-[assembly: AssemblyTitleAttribute("IntegorSharedErrorHandlers")]
-[assembly: AssemblyVersionAttribute("1.0.0")]
--- IntegorSharedResponseDecorators/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompanyAttribute("IntegorSharedResponseDecorators")]
-[assembly: AssemblyConfigurationAttribute("Release")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: AssemblyProductAttribute("IntegorSharedResponseDecorators")]
-[assembly: AssemblyTitleAttribute("IntegorSharedResponseDecorators")]
-[assembly: AssemblyVersionAttribute("1.0.0")]
